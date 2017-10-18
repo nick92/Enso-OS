5907ee562cf5281aa4a53cde13a798a7cb78de73
Nick Wilkins <nickawilkins@hotmail.com>
2017-10-18 09:20:01 +0100

    fix for branch version issue

--- greeter/build/po/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/work/test/Enso-OS/greeter/po
-# Build directory: /home/nick/work/test/Enso-OS/greeter/build/po
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
