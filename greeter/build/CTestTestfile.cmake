3ce7efa4b4b79b2bba2f9ec0ea5eb36372a4b3cf
Nick Wilkins <nickawilkins@hotmail.com>
2017-10-26 13:56:23 +0100

    merge conflicts

---
+++ greeter/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nick/work/Enso-OS/greeter
+# Build directory: /home/nick/work/Enso-OS/greeter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(po)
