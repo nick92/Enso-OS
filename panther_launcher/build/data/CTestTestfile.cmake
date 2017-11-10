9fa923feef8076ca2ed224aab6c1acc4459ae899
Nick Wilkins <nickawilkins@hotmail.com>
2017-11-10 16:33:59 +0000

    add panther launcher img to README

--- panther_launcher/build/data/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/work/Enso-OS/panther_launcher/data
-# Build directory: /home/nick/work/Enso-OS/panther_launcher/build/data
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- panther_launcher/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/work/Enso-OS/panther_launcher/src
-# Build directory: /home/nick/work/Enso-OS/panther_launcher/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
