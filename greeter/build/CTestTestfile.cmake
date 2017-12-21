e163d78e41dbe8722c78fa9f9a03448240ddae75
Nick Wilkins <nickawilkins@hotmail.com>
2017-12-21 21:54:01 +0000

    appending commit

--- greeter/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/work/Enso/greeter
-# Build directory: /home/nick/work/Enso/greeter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(po)
--- greeter/build/po/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/work/Enso/greeter/po
-# Build directory: /home/nick/work/Enso/greeter/build/po
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
