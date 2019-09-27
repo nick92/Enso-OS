b4b1b23641214c365339b41f469c96964b9d39e1
Nick Wilkins <nickawilkins@hotmail.com>
2019-09-27 23:23:26 +0100

    add deps file

---
+++ greeter/deps.sh
@@ -0,0 +1,9 @@
+
+
+sudo apt install libgtk-3-dev \
+                 libgranite-dev \
+                 liblightdm-gobject-1-dev \
+                 libclutter-gtk-1.0-dev \
+                 valac \
+                 meson
+
